f13db002015a5d0065efbf6697b5d170e7b0edb1
Priates of Zimsey <realcoder2023@gmail.com>
2025-10-21 16:14:29 -0400

    added check mark if checked is false or trye set checked to done and show message done

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,37 @@
 import { Fragment } from "react/jsx-runtime";
 import Header from "./Components/Header";
 import "./App.css";
+import { useState } from "react";
 
 export default function App() {
+  const [checkedCurrent, setCheckedCurrent] = useState(false);
+  const [checkedCurrentEnd, setCheckedCurrentEnd] = useState(false);
+  const [checked, setChecked] = useState(false);
+
+  const check = (e: any) => {
+    if (e.target.checked) {
+      console.log("Checked");
+    }
+
+    setCheckedCurrent((current) => !current);
+  };
+
+  const checkDone = (e: any) => {
+    if (e.target.checked) {
+      console.log("Checked");
+    }
+
+    setChecked((current) => !current);
+  };
+
+  const checkDoneEnd = (e: any) => {
+    if (e.target.checked) {
+      console.log("Checked");
+    }
+
+    setCheckedCurrentEnd((current) => !current);
+  };
+
   return (
     <Fragment>
       <Header name="Completed Todo List" />
@@ -10,7 +39,9 @@ export default function App() {
       <div className="container">
         <h2>Step 1:</h2>
         <h2 id="completed">Are Dishes Done?</h2>
-        <input type="checkbox" id="typed" />
+        <input type="checkbox" onChange={check} id="typed" />
+
+        {checkedCurrent && <h3>Done..</h3>}
       </div>
 
       <hr />
@@ -18,14 +49,17 @@ export default function App() {
       <div className="container">
         <h2>Step 2: </h2>
         <h2 id="completed">Did Take Shower?</h2>
-        <input type="checkbox" id="typed" />
+        <input type="checkbox" onChange={checkDone} id="typed" />
+        {checked && <h3>Done..</h3>}
       </div>
       <hr />
 
       <div className="container">
         <h2>Step 3: </h2>
         <h2 id="completed">Did You Do Laundry?</h2>
-        <input type="checkbox" id="typed" />
+        <input type="checkbox" onChange={checkDoneEnd} id="typed" />
+
+        {checkedCurrentEnd && <h3>Done..</h3>}
       </div>
       <hr />
     </Fragment>
